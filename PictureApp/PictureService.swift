b6853f6a9b44e2f05fff5429b3584a972de28849
Irina Gorbachenko <irinagorbachenko23@gmail.com>
2022-01-11 14:47:04 +0200

    Get method is changed to return Result

--- PictureApp/PictureService.swift
+++ PictureApp/PictureService.swift
@@ -14,7 +14,7 @@ public struct PictureService {
         self.session = session
     }
     
-    func fetch(completion: @escaping ([Post], Int) -> (),order : Order ,selectedCategory:[Category],currentPage:Int) {
+    func fetch(order: Order, selectedCategory: [Category], currentPage: Int, completion: @escaping (Result<([Post], Int), Error>) -> ()) {
         
         var components = [
             "key": "8630898-e092bf16cb1dd9ff6a483dabf",
@@ -23,7 +23,6 @@ public struct PictureService {
             "safesearch": "true",
             "page": currentPage,
             "order": order,
-            //"category": selectedCategory.first?.rawValue
         ] as [String : Any]
         
         if let selectedCategory = selectedCategory.first{
@@ -31,14 +30,20 @@ public struct PictureService {
         }
         
         var urlComponents = URLComponents(string: "https://pixabay.com/api")!
-        urlComponents.queryItems = components.map({ URLQueryItem(name: $0, value: "\($1)")})
+        // urlComponents.queryItems = components.map({ URLQueryItem(name: $0, value: "\($1)")})
+        var queryItems = [URLQueryItem]()
+        for (key,value) in components {
+            queryItems.append(URLQueryItem.init(name: key, value: "\(value)"))
+        }
+        urlComponents.queryItems = queryItems
         
         session.get(from: urlComponents.url!) { (result) in
             switch result {
             case let .success(data, response):
-                var totalFromData  = try? JSONDecoder().decode(Hits.self, from: data)
-                completion((try? JSONDecoder().decode(Hits.self, from: data).hits) ?? [] , totalFromData?.totalHits ?? 0)
+                var totalFromData  = try! JSONDecoder().decode(Hits.self, from: data)
+                completion(.success((totalFromData.hits, totalFromData.totalHits)))
             case let .failure(error):
+                completion ( .failure(error))
                 break
             }
         }
